97077ab21c19ce6d5f06b57e317eba81affffca3
SpexID <ngurahadipermana15@gmail.com>
2023-04-22 23:55:31 +0800

    Logo

---
+++ Skintusiast/app/src/main/java/com/example/skintusiast/Start_Logo_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.skintusiast
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Start_Logo_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start_logo)
+    }
+}
