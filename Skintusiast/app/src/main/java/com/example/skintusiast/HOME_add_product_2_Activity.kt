1abb7ae8d33b2e11d4ad41d91669b71c4f4a5ce1
SpexID <ngurahadipermana15@gmail.com>
2023-04-30 01:08:05 +0800

    home add product, need rapi + constraint + intent

---
+++ Skintusiast/app/src/main/java/com/example/skintusiast/HOME_add_product_2_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.skintusiast
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HOME_add_product_2_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_add_product2)
+    }
+}
---
+++ Skintusiast/app/src/main/java/com/example/skintusiast/HOME_add_product_desciption.kt
@@ -0,0 +1,11 @@
+package com.example.skintusiast
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HOME_add_product_desciption : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_add_product_desciption)
+    }
+}
